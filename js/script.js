cba21b21e6c64cf3a60febd05600d29457829eb5
Tamires Luzia Silva <48417347+TLuziaS@users.noreply.github.com>
2020-01-20 09:36:46 -0300

    Versão sem validação dos dados inserido

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+/* Mudança de Tela de Cadastro e Login*/
+let btnLogin = document.querySelector("#login");
+let btnSignup = document.querySelector("#signup");
+let view = document.querySelector("body");
+
+
+btnLogin.addEventListener("click", function () {
+   view.className = "loginJs"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    view.className = "signupJs";
+});
+
+/*Validação dos Dados*/
